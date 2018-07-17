44478ae80cc6cb0b08f20cc68468a83690046599
eternity6666 <eternity6666@qq.com>
2018-07-17 11:04:19 +0800

    add some thing

---
+++ test/structure_string.cpp
@@ -0,0 +1,18 @@
+/*
+ * It is a test for structure and string.
+ */
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+struct stringTest
+{
+    /*
+     * string name;//invalid,you should use "std::string name".
+     */
+    std::string name;
+};
+int main()
+{
+    return 0;
+}

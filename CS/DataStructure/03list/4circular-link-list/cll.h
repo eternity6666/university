436a4994f423b73f3e57611af292a01e8ee0de82
eternity6666 <1462928058@qq.com>
2018-11-06 15:16:02 +0800

    181106

--- CS/DataStructure/03list/4circular-link-list/cll.h
+++ CS/DataStructure/03list/4circular-link-list/cll.h
@@ -1,26 +1,29 @@
-template <typename ElemType>
+#include "../../myhead.h"
+
+template <typename elemtype>
 class circularLinkList
 {
 public:
     class linkNode
     {
     public:
-        ElemType data;
+        elemtype data;
         linkNode *next;
     };
     typedef linkNode * nodeP;
 
     void clear();
 
-    Status deleteElem(int i, ElemType & e);
+    Status deleteElem(int i, elemtype & e);
 
-    Status getHeadElem(ElemType & e);
+    Status getHeadElem(elemtype & e);
 
     bool isEmpty();
 
     Status moveHead(int i);
 
-    circularLinkList<ElemType> operator = (circularLinkList<ElemType> rightL);
+    circularLinkList<elemtype> operator = (circularLinkList<elemtype> rightL);
+    
     circularLinkList();
 
     ~circularLinkList();
@@ -31,4 +34,147 @@ protected:
     nodeP head;
 };
 
+template <typename elemtype>
+void circularLinkList<elemtype>::clear()
+{
+    nodeP p;
+    while(head != head->next)
+    {
+        p = head->next;
+        head->next = p->next;
+        delete p;
+    }
+    delete head;
+    head = NULL;
+}
+
+template <typename elemtype>
+Status circularLinkList<elemtype>::deleteElem(int i, elemtype &e)
+{
+    int j = 1;
+    nodeP r;
+    nodeP p = head;
+
+    if(!head)
+        return ERROR;
+
+    while(j < i)
+    {
+        r = p;
+        p = p->next;
+        ++j;
+    }
+
+    e = p->data;
+
+    if(p == p->next)
+        head = NULL;
+    else
+        head = r->next = p->next;
+
+    delete p;
+    return OK;
+}
+
+template <typename elemtype>
+Status circularLinkList<elemtype>::getHeadElem(elemtype &e)
+{
+    if(!head)
+        return ERROR;
+    e = head->data;
+    return OK;
+}
+
+template <typename elemtype>
+bool circularLinkList<elemtype>::isEmpty()
+{
+    return head ? false : true;
+}
+
+template <typename elemtype>
+Status circularLinkList<elemtype>::moveHead(int i)
+{
+    int j = 1;
+    if(!head)
+        return ERROR;
+
+    while(j < i)
+    {
+        head = head->next;
+        ++j;
+    }
+
+    return OK;
+}
+
+template <typename elemtype>
+circularLinkList<elemtype> circularLinkList<elemtype>::operaotr = (circularLinkList<elemtype> rightL)
+{
+    nodeP p = NULL;
+    nodeP rp = rightL.head;
+    nodeP s;
+    
+    if(this != &rightL)
+    {
+        clear();
+
+        while(rp->next != rightL.head)
+        {
+            s = new linkNode;
+            assert(s != 0);
+            
+            s->data = rp->data;
+
+            if(!head)
+                head = s;
+            else
+                p->next = s;
+            p = s;
+            rp = rp->next;
+        }
+
+        if(head)
+            p->next = head;
+    }
+    return *this;
+}
+
+template <typename elemtype>
+circularLinkList<elemtype>::circularLinkList()
+{
+    head = NULL;
+}
+
+template <typename elemtype>
+circularLinkList<elemtype>::~circularLinkList()
+{
+    clear();
+}
+
+template <typename elemtype>
+circularLinkList<elemtype>::circularLinkList(const circularLinkList &otherL)
+{
+    nodeP p;
+    nodeP op = otherL.head;
+    nodeP s;
+    head = p = NULL;
+
+    while(op != otherL.head)
+    {
+        s = new linkNode;
+        assert(s != 0);
+
+        s->data = op->data;
+
+        if(!head)
+            head = s;
+        else
+            p->next = s;
+        p = s;
+        op = op->next;
+    }
+
+    if(head)
+        p->next = head;
+}
 
--- CS/DataStructure/03list/5double-link-list/dll.h
+++ CS/DataStructure/03list/5double-link-list/dll.h
@@ -1,3 +1,4 @@
+#include "../../myhead.h"
 template<typename ElemType>
 class doubleLinkList
 {
@@ -38,4 +39,265 @@ public:
 
     doubleLinkList(const doubleLinkList<ElemType> &otherL);
 };
+
+template <typename elemtype>
+void doubleLinkList<elemtype>::clear()
+{
+    nodeP r,p;
+    
+    if(!head)
+        return ;
+
+    p = head->prior;
+
+    while(p!=head)
+    {
+        r = p->prior;
+        delete p;
+        p = r;
+    }
+    delete p;
+
+    head = NULL;
+}
+
+template <typename elemtype>
+Status doubleLinkList<elemtype>::deleteElem(elemtype e)
+{
+    nodeP p;
+    
+    if(!locateElem(e,p))
+        return ERROR;
+
+    if(head->next == head)
+        head = NULL;
+    else
+    {
+        if(p == head)
+            head = p->next;
+
+        p->prior->next = p->next;
+        p-next->prior = p->prior;
+    }
+    delete p;
+    return OK;
+}
+
+template <typename elemtype>
+Status doubleLinkList<elemtype>::getElem(int i, elemtype &e)
+{
+    int j = 1;
+    nodeP p = head;
+
+    while(p && p->next != head && j < i)
+    {
+        p = p->next;
+        ++j;
+    }
+
+    if(j == i)
+    {
+        e = p->data;
+        return OK;
+    }
+
+    return ERROR;
+}
+
+template <typename elemtype>
+typename doubleLinkList<elemtype>::nodeP doubleLinkList<elemtype>::getHead()
+{
+    return head;
+}
+
+template <typename elemtype>
+int doubleLinkList<elemtype>::getLength()
+{
+    int length = 0;
+    nodeP p = head;
+
+    while(p)
+    {
+        ++length;
+        p = p->next;
+        if(p == head)
+            break;
+    }
+
+    return length;
+}
+
+template <typename elemtype>
+Status doubleLinkList<elemtype>::insert(int i, elemtype &e)
+{
+    int j = 1;
+    nodeP p = head;
+    nodeP s;
+
+    while(p && p->next != head && j < i)
+    {
+        p = p->next;
+        ++j;
+    }
+
+    if(!head && i != 1 || j < i)
+        return ERROR;
+
+    s = new linkNode;
+    assert(s != 0);
+
+    s->data = e;
+
+    if(i == 1)
+    {
+        if(!head)
+        {
+            head = s->prior = s->next = s;
+            return OK;
+        }
+        head = s;
+    }
+
+    p->prior->next = s;
+    s->prior = p->prior;
+    p->prior = s;
+    s->next = p;
+    
+    return OK;
+}
+
+template <typename elemtype>
+bool doubleLinkList<elemtype>::isEmpty()
+{
+    return head ? false : true;
+}
+
+template <typename elemtype>
+Status doubleLinkList<elemtype>::locateElem(elemtype e, nodeP &r)
+{
+    nodeP p = head;
+
+    while(p && p->next != head && p->data != e)
+        p = p->next;
+
+    if(p->data == e)
+    {
+        r = p;
+        return OK;
+    }
+    else
+        return ERROR;
+}
+
+template <typename elemtype>
+Status doubleLinkList<elemtype>::nextElem(elemtype e, elemtype &next_e)
+{
+    nodeP p;
+    if(locateElem(e, p))
+    {
+        next_e = p->next->data;
+        return OK;
+    }
+    else
+        return false;
+}
+
+template <typename elemtype>
+doubleLinkList<elemtype> doubleLinkList<elemtype>::operator = (doubleLinkList<elemtype> rightL)
+{
+    nodeP p = NULL;
+    nodeP rp = rightL.getHead();
+
+    nodeP s;
+
+    if(this != &rightL)
+    {
+        clear();
+        while(rp)
+        {
+            s = new linkNode;
+            assert(s != 0);
+
+            s->data = rp->data;
+            
+            if(!head)
+                head = p = s;
+            
+            p->next = s;
+            s->prior = p;
+            p = s;
+            rp = rp->next;
+            
+            if(rp == rightL.head)
+                break;
+        }
+        
+        if(head)
+        {
+            head->prior = p;
+            p->next = head;
+        }
+    }
+    return *this;
+}
+
+template <typename elemtype>
+Status doubleLinkList<elemtype>::priorElem(elemtype e, elemtype &prior_e)
+{
+    nodeP p;
+    if(locateElem(e, p))
+    {
+        prior_e = p->prior->data;
+        return OK;
+    }
+    else
+        return ERROR;
+}
+
+template <typename elemtype>
+doubleLinkList<elemtype>::doubleLinkList()
+{
+    head = NULL;
+}
+
+template <typename elemtype>
+doubleLinkList<elemtype>::~doubleLinkList()
+{
+    clear();
+}
+
+template <typename elemtype>
+doubleLinkList<elemtype>::doubleLinkList(const doubleLinkList &otherL)
+{
+    nodeP p;
+    nodeP op = otherL.head;
+    nodeP s;
+    head = p = NULL;
+
+    while(op)
+    {
+        s = new linkNode;
+        assert(s != 0);
+        
+        s->data = op->data;
+        
+        if(!head)
+            head = p = s;
+
+        p->next = s;
+        s->prior = p;
+
+        p = s;
+        op = op->next;
+
+        if(op == otherL.head)
+            break;
+    }
+
+    if(head)
+    {
+        head->prior = p;
+        p->next = head;
+    }
+}
 

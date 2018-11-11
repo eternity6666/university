7b50218919ca0399dd48d6dae332d9439b96867d
eternity6666 <1462928058@qq.com>
2018-11-11 18:30:52 +0800

    181111

--- CS/DataStructure/04stack/3linkStack/linkStack.h
+++ CS/DataStructure/04stack/3linkStack/linkStack.h
@@ -23,7 +23,7 @@ public:
 
     linkStack<ElemType> operator = (linkStack<ElemType> rightS);
 
-    Status pop(ElemType &e);
+    bool pop(ElemType &e);
 
     void push(ElemType e);
 
@@ -73,28 +73,99 @@ bool linkStack<elemtype>::getTop(elemtype &e)
 }
 
 template<typename ElemType>
+bool linkStack<elemtype>::isEmpty()
+{
+    return (top ? false : true);
+}
 
 template<typename ElemType>
+linkStack<elemtype> linkStack<elemtype>::operator = (linkStack<elemtype> rightS)
+{
+    nodeP p;
+    nodeP rp = rightS.top;
+    nodeP s;
+    if(this != &rightS)
+    {
+        clear();
+
+        while(rp)
+        {
+            s = new linkNode;
+            assert(s != 0);
+
+            s->data = rp->data;
+
+            if(!top)
+                top = s;
+            else
+                p->next = s;
+            p = s;
+            rp = rp->next;
+        }
+        if(p)
+            p->next = NULL;
+    }
+    return *this;
+}
 
 template<typename ElemType>
+bool linkStack<elemtype>::pop(elemtype &e)
+{
+    nodeP s = top;
+    if(!top)
+        return false;
+    e = top->data;
+    top = top->next;
+    delete s;
+    return true;
+
+}
 
 template<typename ElemType>
+void linkStack<elemtype>::push(elemtype e)
+{
+    nodeP s;
+    s = new linkNode;
+    assert(s != 0);
+
+    s->data = e;
+    s->next = top;
+    top = s;
+}
 
 template<typename ElemType>
+linkStack<elemtype>::linkStack()
+{
+    top = NULL;
+}
 
 template<typename ElemType>
+linkStack<elemtype>::~linkStack()
+{
+    clear();
+}
 
 template<typename ElemType>
+linkStack<elemtype>::linkStack(const linkStack<elemtype> &otherS)
+{
+    nodeP p = NULL, op = otherS.top, s;
 
-template<typename ElemType>
+    top = NULL;
 
-template<typename ElemType>
+    while(op)
+    {
+        s = new linkNode;
+        assert(s != 0);
 
-template<typename ElemType>
-
-template<typename ElemType>
-
-template<typename ElemType>
-
-template<typename ElemType>
+        s->data = op->data;
+        if(!top)
+            top = s;
+        else
+            p->next = s;
+        p = s;
+        op = op->next;
+    }
+    if(p)
+        p->next = NULL;
+}
 

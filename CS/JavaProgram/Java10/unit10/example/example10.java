0dce33158fce626a53b81bfe103c56a2d41152b4
eternity6666 <1462928058@qq.com>
2019-03-22 23:43:58 +0800

    updated

---
+++ CS/JavaProgram/Java10/unit10/example/example10.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class example10 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = input.nextLine();
+
+        System.out.println("Ignoring nonalphanumeric characters, \n is " + s + " a palindrome? " + isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+
+        String s1 = filter(s);
+    
+        String s2 = reverse(s1);
+        
+        return s2.equals(s1);
+    }
+
+    public static String filter(String s) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i = 0; i < s.length(); i++)
+            if(Character.isLetterOrDigit(s.charAt(i)))
+                stringBuilder.append(s.charAt(i));
+
+        return stringBuilder.toString();
+    }
+
+    public static String reverse(String s) {
+        
+        StringBuilder stringBuilder = new StringBuilder(s);
+        stringBuilder.reverse();
+
+        return stringBuilder.toString();
+    }
+}
+

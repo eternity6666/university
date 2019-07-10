1ea69e5cbc7c136e9602aebb0ec65bf64df113b8
eternity6666 <1462928058@qq.com>
2019-07-10 20:27:36 +0800

    updated

---
+++ CS/Python/learnFromLXF/test1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+s5 = '''Hello,
+baby!'''
+
+print(n)
+print(f)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+print(s5)
